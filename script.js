b8640b2846983df96fff1a6c041f5a12c21e214d
Léon van Velzen <leon-vv@hotmail.com>
2016-04-02 17:12:43 +0200

    Create black, empty cells at the start

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("works");
+
+window.onload = function() {
+
+    (function() {
+
+        var board = document.getElementById("board");
+
+        for(var i = 0; i < 6 * 7; i++) {
+            var cell = document.createElement("div");
+            cell.className = "cell";
+            cell.style.backgroundColor = "#000";
+            board.appendChild(cell);
+        }
+    })();
+
+};
